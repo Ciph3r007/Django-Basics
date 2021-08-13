d47eb8e62d92607eeb40e87e6d8815aa698a901d
Ciph3r007 <79519082+Ciph3r007@users.noreply.github.com>
2021-08-13 15:26:21 +0600

    Q object query

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.db.models import Q
 from store.models import Product
 
 def say_hello(request):
-    query_set = Product.objects.filter(unit_price__range=(20, 30))
+    # price between 20 and 30 and inventory > 10
+    query_set = Product.objects.filter(unit_price__range=(20, 30), inventory__gt=10)
+    # price not between 20 and 30 and inventory > 10
+    query_set = Product.objects.filter(~Q(unit_price__range=(20, 30)) | Q(inventory__gt=10))
 
     for product in query_set:
         print(product)
