1697b91e3871c35b305ea30a66d6f12194a07bc4
Ciph3r007 <79519082+Ciph3r007@users.noreply.github.com>
2021-08-14 12:55:31 +0600

    Register models with decorator and customize list

--- store/admin.py
+++ store/admin.py
@@ -2,5 +2,17 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['Customer', 'first_name', 'last_name', 'membership']
+    ordering = ['first_name', 'last_name']
+    list_editable = ['membership']
+    list_per_page = 20
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'unit_price']
+    list_editable = ['unit_price']
+    list_per_page = 20
+
 admin.site.register(models.Collection)
-admin.site.register(models.Product)
