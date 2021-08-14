87c0560caf76afb4e0f95e5b349cca6a09c0ef9e
Ciph3r007 <79519082+Ciph3r007@users.noreply.github.com>
2021-08-14 13:27:29 +0600

    Retreive related relations, Register Customer model

--- store/admin.py
+++ store/admin.py
@@ -11,14 +11,29 @@ class CustomerAdmin(admin.ModelAdmin):
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price', 'inventory_status']
+    list_display = ['title', 'unit_price', 'inventory_status', 'collection_name']
     list_editable = ['unit_price']
     list_per_page = 20
+    # Displaying 'collection' directly is much better. Needed only when retrieving unavailable fields
+    list_select_related = ['collection'] 
 
     @admin.display(ordering='inventory')
     def inventory_status(self, product):
         if product.inventory < 10:
             return 'Low'
         return 'OK'
+    
+    # Displaying 'collection' directly is much better. Needed only when retrieving unavailable fields
+    @admin.display(ordering='collection')
+    def collection_name(self, product):
+        return product.collection.title
+    
+
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'placed_at', 'payment_status', 'customer']
+    ordering = ['-placed_at']
+    list_per_page = 20
 
 admin.site.register(models.Collection)
