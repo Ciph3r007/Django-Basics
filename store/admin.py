af7b05ef0e2753eef5371b506a63df0bf8e073ad
Ciph3r007 <79519082+Ciph3r007@users.noreply.github.com>
2021-08-14 14:51:07 +0600

    Add filters for products

--- store/admin.py
+++ store/admin.py
@@ -1,10 +1,28 @@
 from django.contrib import admin
 from django.db.models.aggregates import Count
+from django.db.models.query import QuerySet
 from django.urls import reverse
 from django.utils.html import format_html, urlencode
 from . import models
 
-# Register your models here.
+class InventoryFilter(admin.SimpleListFilter):
+    title='inventory' # Shown as 'By inventory'
+    parameter_name = 'inventory' # Shown in the api link 
+
+    def lookups(self, request, model_admin):
+        filters = [
+            ('<10', 'Low'),
+            ('>=10', 'OK')
+        ]
+        return filters
+    
+    def queryset(self, request, queryset: QuerySet):
+        if self.value() == '<10':
+            return queryset.filter(inventory__lt=10)
+        if self.value() == '>=10':
+            return queryset.filter(inventory__gte=10)
+
+# Model Registering
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'membership', 'orders_count']
@@ -31,6 +49,7 @@ class CustomerAdmin(admin.ModelAdmin):
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'unit_price', 'inventory_status', 'collection_name']
     list_editable = ['unit_price']
+    list_filter = ['collection', 'last_update', InventoryFilter]
     list_per_page = 20
     # Displaying 'collection' directly is much better. Needed only when retrieving unavailable fields
     list_select_related = ['collection']
